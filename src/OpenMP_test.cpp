1254744197724b9e55f6b7628524731e673ba8a0
SugarWind <g2127037@st.t-kougei.ac.jp>
2021-12-12 22:13:48 +0900

    並列化しました。

--- src/OpenMP_test.cpp
+++ src/OpenMP_test.cpp
@@ -23,6 +23,7 @@ bool monochrome(const char* filename)
     auto start = system_clock::now();// 時間計測用：気にしないこと
 
     // ■ OpenMPを使って並列化してください。
+#pragma omp parallel for
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             unsigned char* r = &pixels[(y * width + x) * bpp + 0];
@@ -49,7 +50,7 @@ bool monochrome(const char* filename)
 }
 
 // ぼかす
-bool blur(const char *filename, int num)
+bool blur(const char* filename, int num)
 {
     int width, height; //画像サイズの格納先
     int bpp; //一画素のバイト数
@@ -61,6 +62,7 @@ bool blur(const char *filename, int num)
 
     // ■ OpenMPを使って並列化してください。
     // 依存性があり、並列化すると処理の順番によって結果が変わる可能性があるので、変わらないように注意すること
+#pragma omp parallel for 
     for (int i = 0; i < num; i++) {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
